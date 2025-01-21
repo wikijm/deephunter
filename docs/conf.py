3ee48c194df37632cdab45398ffe60971bd94c59
Sebastien DAMAYE <sebastien.damaye@se.com>
2025-01-21 15:07:55 +0100

    updated doc

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ author = 'Sébastien Damaye'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-extensions = []
+extensions = ['sphinx.ext.autosectionlabel']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
