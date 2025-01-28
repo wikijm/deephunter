e8ab48e6f33be4f88644f5ceaf779c37d4b856f3
Sebastien DAMAYE <sebastien.damaye@se.com>
2025-01-28 08:14:49 +0100

    adding a script to update S1 token

---
+++ qm/scripts/update_s1_token.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if token is passed as arg
+if [ -z "$1" ]; then
+  echo "Error: Please provide TOKEN as argument."
+  exit 1
+fi
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+SETTINGS_PATH="$SCRIPT_DIR/../../deephunter/settings.py"
+
+# Update S1 token in configuration file
+sed -i "s/TOKEN = '[^']*'/TOKEN = '$1'/g" $SETTINGS_PATH
+
+# Restart services
+sudo systemctl restart apache2
+sudo systemctl restart celery
+sudo systemctl restart redis
+
