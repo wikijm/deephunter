3b3a9feb634c45245173dac7409281067ab49345
Sebastien DAMAYE <sebastien.damaye@se.com>
2025-02-27 14:35:38 +0100

    FR #41 - Sync STAR rule flag with rules in S1

--- qm/apps.py
+++ qm/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class QmConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'qm'
+
+    def ready(self):
+        # This will import the signals when the app is ready
+        import qm.signals
---
+++ qm/signals.py
@@ -0,0 +1,81 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.conf import settings
+from .models import Query
+import requests
+import json
+
+# Params for requests API calls
+S1_URL = settings.S1_URL
+S1_TOKEN = settings.S1_TOKEN
+PROXY = settings.PROXY
+
+# This handler is triggered after an object is saved (created or updated)
+@receiver(post_save, sender=Query)
+def post_save_handler(sender, instance, created, **kwargs):
+    # only apply if STAR_RULE flag set
+    if instance.star_rule:
+
+        # filter "tenant=true" is to apply rule to scope "global"
+        body = {
+            "data": {
+                "queryLang": "2.0",
+                "severity": "High",
+                "description": "Rule Sync from DeepHunter",
+                "s1ql": instance.query,
+                "name": instance.name,
+                "queryType": "events",
+                "expirationMode": "Permanent",
+                "status": "Active"
+            },
+            "filter": {
+                "tenant": "true"
+            }
+        }
+
+        # For newly created analytic
+        if created:
+            r = requests.post(f'{S1_URL}/web/api/v2.1/cloud-detection/rules',
+                json=body,
+                headers={'Authorization': f'ApiToken:{S1_TOKEN}'},
+                proxies=PROXY
+                )
+                
+        # When analytic is updated
+        else:
+            # check if STAR rule already exists (STAR rule flag was previously set)
+            r = requests.get(f'{S1_URL}/web/api/v2.1/cloud-detection/rules?name__contains={instance.name}',
+                headers={'Authorization': f'ApiToken:{S1_TOKEN}'},
+                proxies=PROXY
+                )
+            if 'data' in r.json():
+                # if it exists, update it
+                rule_id = r.json()['data'][0]['id']
+                r = requests.put(f'{S1_URL}/web/api/v2.1/cloud-detection/rules/{rule_id}',
+                    json=body,
+                    headers={'Authorization': f'ApiToken:{S1_TOKEN}'},
+                    proxies=PROXY
+                    )                
+            else:
+                # if it does not exist (STAR rule flag was not set), create it
+                r = requests.post(f'{S1_URL}/web/api/v2.1/cloud-detection/rules',
+                    json=body,
+                    headers={'Authorization': f'ApiToken:{S1_TOKEN}'},
+                    proxies=PROXY
+                    )
+
+# This handler is triggered after an object is deleted
+@receiver(post_delete, sender=Query)
+def post_delete_handler(sender, instance, **kwargs):
+    # only apply if STAR_RULE flag set
+    if instance.star_rule:
+        body = {
+            "filter": {
+                "name__contains": instance.name
+            }
+        }
+        r = requests.delete(f'{S1_URL}/web/api/v2.1/cloud-detection/rules',
+            json=body,
+            headers={'Authorization': f'ApiToken:{S1_TOKEN}'},
+            proxies=PROXY
+            )
